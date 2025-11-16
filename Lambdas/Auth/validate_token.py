9e935d8ff0f13c16b52ff6ccff56ba15ef336f02
IgnacioVida1 <ignacio.vidal@utec.edu.pe>
2025-11-16 11:19:06 -0500

    Update validate_token.py

--- Lambdas/Auth/validate_token.py
+++ Lambdas/Auth/validate_token.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     token = event['token']
     # Proceso
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('t_tokens')
+    table = dynamodb.Table('t_token')
     response = table.get_item(
         Key={
             'token': token
